3db8072bce8239cb9466e852250c649991cc878c
Dong Yuan <v-doyuan@microsoft.com>
2018-08-12 01:59:14 +0000

    sync

--- src/dag_controller.py
+++ src/dag_controller.py
@@ -110,7 +110,7 @@ class DagController:
         hooks = [checkpoint_saver_hook]
         if FLAGS.child_sync_replicas:
             sync_replicas_hook = child_ops["optimizer"].make_session_run_hook(True)
-            hooks.append(sync_replicas_hook)
+        #     hooks.append(sync_replicas_hook)
         if FLAGS.controller_training and FLAGS.controller_sync_replicas:
             sync_replicas_hook = generator_ops["optimizer"].make_session_run_hook(True)
             hooks.append(sync_replicas_hook)
